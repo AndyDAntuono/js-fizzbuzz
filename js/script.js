34ff58380df5310eeb42553b6f93cb50799bfb5c
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-27 17:24:28 +0200

    [UPD] script.js : for fixed

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ for (let i = 1; i < 10; i++) {
 
 for (let i=1; i <= 100; i++){
     console.log(i);
-    if( i % 3 && i % 5 ) {
+    if( i % 3 == 0 && i % 5 == 0 ) {
         console.log(`${i}FizzBuzz`);
     } 
     
