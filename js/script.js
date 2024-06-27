2ad2009222a96af0870b3905008040f9c8e3985c
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-27 18:08:02 +0200

    [UPD] added comments to the readme file

--- js/script.js
+++ js/script.js
@@ -7,16 +7,18 @@ for (let i = 1; i < 10; i++) {
 }
 */
 
+//calcolo un for che rappresenti un ciclo che parta dal numero fino al numero 100
 for (let i=1; i <= 100; i++){
     console.log(i);
+    //faccio in moddo che numero che sia multiplo di 3 che di 5 venga costrasegnato dalla parola Fizzbuzz
     if( i % 3 == 0 && i % 5 == 0 ) {
         console.log(`${i}FizzBuzz`);
     } 
-    
+    //faccio in moddo che numero che sia multiplo di 3 venga costrasegnato dalla parola Fizz
     else if ( i % 3 == 0 ) {
         console.log(`${i}Fizz`);
     }
-        
+    //faccio in moddo che numero che sia multiplo di 5 venga costrasegnato dalla parola Buzz   
     else if( i % 5 == 0 ) {
         console.log(`${i}Buzz`);
     }
