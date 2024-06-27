59b3a36ed0983bff27270468f6616219e80b9968
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-27 19:30:01 +0200

    [UPD] updated style.css and script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-/*
-for (let i = 1; i < 10; i++) {
-    console.log(i);
-    if(i % 2==0){
-        console.log
-    }
-}
-*/
+//RECUOERO LA SCHACCHIERA
+const fizzbuzzdom = document.querySelector(`.fizzbuzzdom`)
 
 //calcolo un for che rappresenti un ciclo che parta dal numero fino al numero 100
 for (let i=1; i <= 100; i++){
     console.log(i);
-    //faccio in moddo che numero che sia multiplo di 3 che di 5 venga costrasegnato dalla parola Fizzbuzz
+
+    //CREAO IL QUADRATP E AGGIUNGO LA CLASSE square
+    const square = document.createElement(`div`)
+    square.classList.add(`square`)
+    square.classList.add(`common-bg`)
+    square.innerText = `${i}\n`
+
+
+    //faccio in moddo che numero che sia multiplo di 3 che di 5 venga costrasegnato dalla parola Fizzbuz
     if( i % 3 == 0 && i % 5 == 0 ) {
         console.log(`${i}FizzBuzz`);
     } 
@@ -22,4 +24,6 @@ for (let i=1; i <= 100; i++){
     else if( i % 5 == 0 ) {
         console.log(`${i}Buzz`);
     }
+
+    fizzbuzzdom.append(square);
 }
